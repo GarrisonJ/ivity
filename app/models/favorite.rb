6fd591aac76f0b3891e0119eb5f70e3e11173dbb
Garrison Jensen <garrison.jensen@gmail.com>
2012-12-26 22:12:16 -0800

    add validations for favorites

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,27 +1,26 @@
 class Favorite < ActiveRecord::Base
   attr_accessible :user_id, :happening_id
 
-  # before_save :associate_user
+  before_save :associate_user
 
   belongs_to :happening
   belongs_to :user
 
   # Validations
-  validates :user_id, :presence => true
+  validates :user_id, :presence => true, :uniqueness => { :scope => :happening_id }
   validates :happening_id, :presence => true
-  # TODO: validates the uniqueness of the favorite. A user can only favorite a happening once. 
 
   def has_favorite? product
     favorites.find_by_happening_id happening.id
   end
   # Callbacks
-  # protected
+  protected
 
-  # def associate_user
-  #   unless self.user_id
-  #     return self.user_id = session[:user_id] if session[:user_id]
-  #     return false
-  #   end
-  # end
+  def associate_user
+    unless self.user_id
+      return self.user_id = current_user.user_id if current_user.user_id
+      return false
+    end
+  end
 
  end
