ae34565dc1f3f960ccc5a952aa51a3b26bff43e0
garrison jensen <garrison.jensen@gmail.com>
2013-01-19 15:42:00 -0800

    Added paginate to profiles.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
   	@user = User.find_by_profile_name(params[:id])
   	if @user 
         @favorites = @user.favorites
-  			@happenings = @user.happenings.all
+  			@happenings = @user.happenings.page(params[:page]).per(5)
   			render :action => :show
   	else
   		render :file => 'public/404', :status => 404, :fommats => [:html]
