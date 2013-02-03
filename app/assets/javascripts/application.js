89817d5ccf89575bba659edfd29f9f3b6d670eae
garrison jensen <garrison.jensen@gmail.com>
2013-02-03 15:34:48 -0800

    Bootstrap datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,9 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require_tree .
 $(".favorite").click(function() {
   var happening_id = $(this).attr('id');
